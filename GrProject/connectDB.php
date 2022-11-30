f640307319429cbaa8d6a2e43b3f078a435853a1
VNishitha <nishithareddyv888@gmail.com>
2022-11-29 18:25:08 -0600

    Initial commit

---
+++ GrProject/connectDB.php
@@ -0,0 +1,16 @@
+<!--Author Name: Manojreddy minumula-->
+<?php
+
+$servername = "localhost";
+$user = "root";
+$pass = "" ;
+$db = "opgrs";
+
+$connect=mysqli_connect($servername, $user, $pass, $db);
+
+if(!$connect){
+    die ("Connection failed:". mysqli_connect_error());
+}
+
+
+?>
---
+++ GrProject/connection.php
@@ -0,0 +1,11 @@
+<!--Author Name: Manojreddy minumula-->
+<?php
+    
+    $conect = new mysqli('localhost','root','','opgrs');
+    if(!$connect)
+    {
+        die(mysqli_error($connect));
+        
+    }
+
+?>
